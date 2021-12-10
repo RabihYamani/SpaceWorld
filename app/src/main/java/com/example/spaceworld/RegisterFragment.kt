071b23a46e38b68f2a66a6843789b78fcaf469d6
rabih yamani <rabih.yamani@lau.edu>
2021-12-11 00:20:59 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/java/com/example/spaceworld/PhotosAdapter.kt
    #	app/src/main/java/com/example/spaceworld/RoverCameraModel.kt
    #	app/src/main/java/com/example/spaceworld/RoverPhotoModel.kt
    #	app/src/main/java/com/example/spaceworld/RoverPhotos.kt
    #	app/src/main/java/com/example/spaceworld/RoverRepository.kt
    #	app/src/main/res/layout/rover_photo_item.xml

--- app/src/main/java/com/example/spaceworld/RegisterFragment.kt
+++ app/src/main/java/com/example/spaceworld/RegisterFragment.kt
@@ -5,23 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 
 
-class LoginFragment : Fragment() {
+class RegisterFragment : Fragment() {
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_login, container, false)
 
-        view.findViewById<Button>(R.id.btn_register).setOnClickListener(){
-
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
 
